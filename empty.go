0ba9905c7c3fc3fb3fc791e2bb09c7e4d6cef10d
jiangxun <jiangxun@jiandan100.cn>
2023-07-17 11:46:00 +0800

    [feat]:add good

--- empty.go
+++ empty.go
@@ -5,3 +5,7 @@ import "fmt"
 func empty() {
 	fmt.Println("this is a new empty file")
 }
+
+func empty2() {
+	fmt.Println("this is the second empty file")
+}
---
+++ good.go
@@ -0,0 +1,7 @@
+package termiliya_github_io
+
+import "fmt"
+
+func GiveGood() {
+	fmt.Println("please give me the good")
+}
